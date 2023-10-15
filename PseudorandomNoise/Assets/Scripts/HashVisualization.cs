a46ba6dfbd9ed2c3ee9f8e9bf964a1f0570020ba
rockgong <rockgong1987@sina.com>
2023-10-15 18:00:21 +0800

    add postions

--- PseudorandomNoise/Assets/Scripts/HashVisualization.cs
+++ PseudorandomNoise/Assets/Scripts/HashVisualization.cs
@@ -12,21 +12,16 @@ public class HashVisualization : MonoBehaviour
     [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
     struct HashJob : IJobFor
     {
+        [ReadOnly]
+        public NativeArray<float3> positions;
         [WriteOnly]
         public NativeArray<uint> hashes;
-
-        public int resolution;
-        public float invResolution;
         public SmallXXHash hash;
         public float3x4 domainTRS;
 
         public void Execute(int index)
         {
-            float vf = floor(invResolution * index  + 0.00001f);
-            float uf = invResolution * (index - resolution * vf + 0.5f) - 0.5f;
-            vf = invResolution * (vf + 0.5f) - 0.5f; ;
-
-            float3 p = mul(domainTRS, float4(uf, 0.0f, vf, 1.0f));
+            float3 p = mul(domainTRS, float4(positions[index], 1.0f));
             int u = (int)floor(p.x);
             int v = (int)floor(p.z);
             int w = (int)floor(p.y);
@@ -37,6 +32,7 @@ public class HashVisualization : MonoBehaviour
 
     static int
         hashesId = Shader.PropertyToID("_Hashes"),
+        positionsId = Shader.PropertyToID("_Positions"),
         configId = Shader.PropertyToID("_Config");
 
     [SerializeField]
@@ -61,7 +57,8 @@ public class HashVisualization : MonoBehaviour
     };
 
     NativeArray<uint> hashes;
-    ComputeBuffer hashesBuffer;
+    NativeArray<float3> positions;
+    ComputeBuffer hashesBuffer, positionsBuffer;
 
     MaterialPropertyBlock propertyBlock;
 
@@ -69,28 +66,36 @@ public class HashVisualization : MonoBehaviour
     {
         int length = resolution * resolution;
         hashes = new NativeArray<uint>(length, Allocator.Persistent);
+        positions = new NativeArray<float3>(length, Allocator.Persistent);
         hashesBuffer = new ComputeBuffer(length, 4);
+        positionsBuffer = new ComputeBuffer(length, 3 * 4);
+
+        JobHandle handle = Shapes.Job.ScheduleParallel(positions, resolution, transform.localToWorldMatrix, default);
 
         new HashJob {
+            positions = positions,
             hashes = hashes,
-            resolution = resolution,
-            invResolution = 1f / resolution,
             hash = SmallXXHash.Seed(seed),
             domainTRS = domain.Matrix
-        }.ScheduleParallel(hashes.Length, resolution, default).Complete();
+        }.ScheduleParallel(hashes.Length, resolution, handle).Complete();
 
         hashesBuffer.SetData(hashes);
+        positionsBuffer.SetData(positions);
 
         propertyBlock ??= new MaterialPropertyBlock();
         propertyBlock.SetBuffer(hashesId, hashesBuffer);
+        propertyBlock.SetBuffer(positionsId, positionsBuffer);
         propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution, verticalOffset / resolution));
     }
 
     private void OnDisable()
     {
         hashes.Dispose();
+        positions.Dispose();
         hashesBuffer.Release();
+        positionsBuffer.Release();
         hashesBuffer = null;
+        positionsBuffer = null;
     }
 
     private void OnValidate()
